842bbe5bf261ed1291ab20e88deb97b9a3639001
Marcelo Silva <marcelo.silva@luizalabs.com>
2020-01-25 20:35:07 -0300

    Created methods POST and PUT in users endpoint

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,5 +1,12 @@
 import * as  mongoose from 'mongoose';
 
+//Interface utilizada para delcarar os atributos dos nossos documentos
+export interface User extends mongoose.Document {
+    name: string
+    email: string
+    password: string
+}
+
 //Schema informar o mongoose quais são os metadatas dos nossos documentos
 const userSchema = new mongoose.Schema({
     name: {
@@ -16,4 +23,4 @@ const userSchema = new mongoose.Schema({
 })
 
 //Model é o responsavel por manipular os dados do mongoDB
-export const User = mongoose.model('User', userSchema)
+export const User = mongoose.model<User>('User', userSchema)
--- src/users/users.router.ts
+++ src/users/users.router.ts
@@ -3,27 +3,53 @@ import * as restify from 'restify'
 import { User } from './users.model'
 
 class UsersRouter extends Router {
-    applyRoutes(application: restify.Server) {
-        
-        application.get('/users', (req, resp, next) => {
-            User.find().then(users => {
-                resp.json(users)
-                return next()
-            })
-        })
+  applyRoutes(application: restify.Server) {
 
-        application.get('/users/:id', (req, resp, next) => {
-            User.findById(req.params.id).then(user => {
-                if (user) {
-                    resp.json(user)
-                    return next()
-                }
-                resp.send(404)
-                return next()
-            })
-        })
+    application.get('/users', (req, resp, next) => {
+      User.find().then(users => {
+        resp.json(users)
+        return next()
+      })
+    })
 
-    }
+    application.get('/users/:id', (req, resp, next) => {
+      User.findById(req.params.id).then(user => {
+        if (user) {
+          resp.json(user)
+          return next()
+        }
+        resp.send(404)
+        return next()
+      })
+    })
+
+
+    application.post('/users', (req, resp, next) => {
+      let user = new User(req.body)
+      user.save().then((user) => {
+        user.password = undefined
+        resp.json(user)
+        return next()
+      })
+    })
+
+    application.put('/users/:id', (req, resp, next) => {
+      //Utiliza-se overwrite para indicar o mongoose se faz o update completo do document ou parcialmente
+      const options = { overwrite: true } 
+      User.update({ _id: req.params.id }, req.body, options)
+        .exec().then(result => {
+          if (result.n) {
+            return User.findById(req.params.id) as any
+          } else {
+            resp.send(404)
+          }
+        }).then(user => {
+          resp.json(user)
+          return next()
+        })
+    })
+
+  }
 }
 
 export const usersRouter = new UsersRouter()
